45abf2f7a1418cdcfe580dc83789ead4079570ff
Thaygo Bajgar <tbrduarte@gmail.com>
2024-04-17 17:49:56 -0300

    fix: remove unused imports

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { UserContext } from "./providers/UserContext";
+// import { useContext } from "react";
+// import { UserContext } from "./providers/UserContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { DefaultTemplate } from "./components/DefaultTemplate";
